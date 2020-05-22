5a1b29b1df4b274d7f6e335244f03f6fd9d7115a
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2020-05-22 16:11:14 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// Main js file
+// see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+import '../../node_modules/jquery/dist/jquery.min'
+import '../../node_modules/bootstrap/dist/js/bootstrap.min'
+import '../../node_modules/popper.js/dist/popper.min'
+
